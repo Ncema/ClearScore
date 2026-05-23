9a25fb55fa17b4a5a91c921b80e3c6918a01053a
Rider <marth.mtsweni@gmail.com>
2026-05-23 15:54:29 +0200

    Updated the comments on the Data service class
     Updated the comments on the Data service class

--- ClearScore/Server/DataService.swift
+++ ClearScore/Server/DataService.swift
@@ -11,38 +11,35 @@ final class DataService: DataServiceProtocol {
     // func fetchData(path: String,completion: @escaping (Result<T, Error>) -> Void)
     func fetchData<T: Decodable>(path: String,completion: @escaping (Result<T, Error>) -> Void){
         
-        // 1. Convert string path into a valid URL
+        //Convert string path into a valid URL
         guard let url = URL(string: path) else {
             completion(.failure(NSError(domain: "Invalid URL", code: -1)))
             return
         }
         
-        // 2. Create and start a network request using URLSession
+        //Create and start a network request using URLSession
         URLSession.shared.dataTask(with: url) { data, response, error in
             
-            // 3. Check if request failed (network error, no internet, etc.)
+            // Check if request failed (network error, no internet, etc.)
             if let error = error {
                 completion(.failure(error))
                 return
             }
             
-            // 4. Check if server returned data
+            //Check if server returned data
             guard let data = data else {
                 completion(.failure(NSError(domain: "NoData", code: -1)))
                 return
             }
             
-            // 5. Try to decode JSON into your Swift model
+            // Try to decode JSON into your model
             do {
                 let decodedData = try JSONDecoder().decode(T.self, from: data)
-                
-                // 6. Return success with decoded model
                 completion(.success(decodedData))
                 
             } catch {
-                // 7. If decoding fails, return error
                 completion(.failure(error))
             }
-        }.resume() // 8. Start the network request
+        }.resume() //Start the network request
     }
 }
